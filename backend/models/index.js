e47d5e88e79701f68329aa8c5664246083aef616
SabaAbramishvili <abramishvilisabu@gmail.com>
2023-09-23 10:33:30 +0400

    update

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,4 +1,4 @@
-const { Sequelize, DataTypes } = require("Sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
